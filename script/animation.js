2f321903fa27a211c0937db56453861da3caefdb
dave192003 <lebicodavedarylpajares09@gmail.com>
2026-06-08 22:56:38 +0800

    added script folder

---
+++ script/animation.js
@@ -0,0 +1,17 @@
+function initScrollAnimation() {
+  const cards = document.querySelectorAll(".card-item");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  }, {
+    threshold: 0.2
+  });
+
+  cards.forEach(card => observer.observe(card));
+}
+
+initScrollAnimation();
